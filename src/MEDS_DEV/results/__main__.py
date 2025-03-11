d745e6228141c39d41509a393d5ba3dc6902865d
Matthew McDermott <mattmcdermott8@gmail.com>
2025-03-11 17:51:13 -0400

    Added starter code for result packaging

---
+++ src/MEDS_DEV/results/__main__.py
@@ -0,0 +1,27 @@
+import json
+import logging
+from pathlib import Path
+
+import hydra
+from omegaconf import DictConfig
+
+from . import CFG_YAML, Result
+
+logger = logging.getLogger(__name__)
+
+
+@hydra.main(version_base=None, config_path=str(CFG_YAML.parent), config_name=CFG_YAML.stem)
+def main(cfg: DictConfig):
+    """Package the result of a MEDS-DEV experiment."""
+
+    eval_fp = Path(cfg.evaluation_fp)
+    if not eval_fp.is_file():
+        raise FileNotFoundError(f"File not found: {eval_fp}")
+
+    eval_result = json.loads(eval_fp.read_text())
+    timestamp = eval_fp.stat().st_mtime
+
+    result = Result(
+        dataset=cfg.dataset, task=cfg.task, model=cfg.model, result=eval_result, timestamp=timestamp
+    )
+    result.to_json(cfg.result_fp, do_overwrite=cfg.get("do_overwrite", False))
